5698cbe955255fec93448a8eb2ab21cf7cf9817d
bihterkuscu <elifbihterk@hotmail.com>
2022-03-06 04:03:52 +0300

    edited program

---
+++ diziler_array_sinifi_metodlari/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+ class program
+{
+    public static void Main(string[] args)
+    {
+        //sort
+        int[] sayiDizisi ={23,12,4,86,72,3,11,17};
+        Console.WriteLine ("**Sirasiz Dizi**");
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine (sayi);
+
+        Console.WriteLine ("**Sirali Dizi**");
+        Array.Sort(sayiDizisi);
+        //Clear
+        Console.WriteLine("*Array Clear**");
+        Array.Clear(sayiDizisi,2,2);
+        foreach(var sayi in sayiDizisi)
+        Console.WriteLine(sayi);
+        //Reverse
+        Console.WriteLine("Array Reverse");
+        Array.Reverse(sayiDizisi);
+        foreach(var sayi in sayiDizisi)
+          Console.WriteLine(sayi);
+        //İndexOf
+        Console.WriteLine("Array İndexOf");
+        Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+        //Resize
+        Console.WriteLine("Array Resize");
+        Array.Resize<int>(ref sayiDizisi,9);
+        sayiDizisi[8]=99;
+        foreach(var sayi in sayiDizisi)
+         Console.WriteLine(sayi);
+
+
+       }
+    }
+
