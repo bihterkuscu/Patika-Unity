94440f0d341759ac62c7477d727b4f23c9f5dbe4
bihterkuscu <elifbihterk@hotmail.com>
2022-03-06 05:03:59 +0300

    edited program

---
+++ metotlar_overloading/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+ class program
+{
+    static void Main(string[] args)
+    {
+        string sayi ="999";
+        bool sonuc = int.TryParse(sayi,out int outSayi);
+        if(sonuc){
+            Console.WriteLine("Basarili");
+            Console.WriteLine(outSayi);
+        }
+            else
+            {
+                Console.WriteLine("Basarisiz");
+            }
+            Metotlar instance =new Metotlar();
+            instance.Topla(4,5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+            //Metot Aşırı Yükleme --Overloading
+            int ifade =999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Zikriye","Ürkmez");
+        }
+    }
+class Metotlar
+{
+    public void Topla(int a, int b, out int toplam){
+        toplam=a+b;
+    }
+    public void EkranaYazdir(string veri){
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri){
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(string veri1, string veri2){
+        Console.WriteLine(veri1+ veri2);
+       }
+     }
+
+
