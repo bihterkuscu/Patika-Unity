cb05a85eb4ae94e5f4633bbc059ad5cd0f0da59f
bihterkuscu <elifbihterk@hotmail.com>
2022-03-08 02:36:13 +0300

    added Odev_2 Soru_3

--- Odev_2/Soru_3/Program.cs
+++ Odev_2/Soru_3/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+
+public class FindVowels
+{
+    public static void Main(string[] args)
+    {
+        var vowels = new List<char>() {'a', 'e', 'i', 'o', 'u'};
+        var vowelsFound = new List<char>();
+
+        Console.WriteLine("Metni Giriniz:");
+        string sentence = Console.ReadLine();
+		
+        foreach (char c in sentence)
+        {
+            if (vowels.Contains(c))
+            {
+                vowelsFound.Add(c);
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
